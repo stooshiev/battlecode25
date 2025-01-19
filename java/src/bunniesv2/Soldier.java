89f17c30c8223d9d0d6c7eff2565210d944ad778
Darren Kao <nobelium100@gmail.com>
2025-01-19 13:35:52 -0500

    Minor changes

--- java/src/bunniesv2/Soldier.java
+++ java/src/bunniesv2/Soldier.java
@@ -1,46 +1,47 @@
 package bunniesv2;
 
-import battlecode.common.*;
+import battlecode.common.*; import java.util.*;
 
 public class Soldier extends RobotPlayer {
-static MapLocation towerMain = null;
+	static MapLocation towerMain = null;
 
-public static Direction getShortestPathDir(RobotController rc, MapLocation goal) {
-    Direction straightDir = rc.getLocation().directionTo(goal);
-    Direction nextDir = straightDir;
-    
-    if (rc.canMove(nextDir)) {
-    return nextDir;
-    }
-    
-    nextDir = straightDir.rotateLeft();
-    
-    if (rc.canMove(nextDir)) {
-    return nextDir;
-    }
-    
-    nextDir = straightDir.rotateRight();   
-    
-    if (rc.canMove(nextDir)) {
-    return nextDir;
-    }
-    
-    return Direction.CENTER;
+	public static Direction getShortestPathDir(RobotController rc, MapLocation goal) {
+	    Direction straightDir = rc.getLocation().directionTo(goal);
+	    Direction nextDir = straightDir;
+	    
+	    if (rc.canMove(nextDir)) {
+	    return nextDir;
+	    }
+	    
+	    nextDir = straightDir.rotateLeft();
+	    
+	    if (rc.canMove(nextDir)) {
+	    return nextDir;
+	    }
+	    
+	    nextDir = straightDir.rotateRight();   
+	    
+	    if (rc.canMove(nextDir)) {
+	    return nextDir;
+	    }
+	    
+	    return Direction.CENTER;
     }
 
-public static MapLocation getTowerLoc() {
-return towerMain;
-}
+	public static MapLocation getTowerLoc() {
+		return towerMain;
+	}
 
-public static void setTowerLoc(MapLocation newTowerLoc) {
-towerMain = newTowerLoc;
-}
+	public static void setTowerLoc(MapLocation newTowerLoc) {
+		towerMain = newTowerLoc;
+	}
 
-public static boolean checkTowerLoc(RobotController rc, MapInfo newTower) throws GameActionException {
-if (rc.getTeam() == rc.senseRobotAtLocation(newTower.getMapLocation()).getTeam()) {
-setTowerLoc(newTower.getMapLocation());
-return true;
-}
-return false;
-}
+	public static boolean checkTowerLoc(RobotController rc, MapInfo newTower) throws GameActionException {
+		if (rc.getTeam() == rc.senseRobotAtLocation(newTower.getMapLocation()).getTeam()) {
+			setTowerLoc(newTower.getMapLocation());
+			return true;
+		}
+		return false;
+	}
+
 }
